8080b55cae418f0c5e4cfeb3d7f394f996f9ed53
= <lucas.bent@maine.edu>
2023-09-16 17:14:51 -0400

    test

--- be/src/server.js
+++ be/src/server.js
@@ -37,6 +37,7 @@ server.listen(Number(process.env.PORT), () => {
 app.post('/api/company', async (req, res) => {
     try {
         const response = await Data.read(req.body.company)
+        response._source['Grade'] = grade(response._source)
         return res.status(200).send(response)
     }
     catch (e) {
@@ -47,9 +48,52 @@ app.post('/api/company', async (req, res) => {
 app.post('/api/search', async (req, res) => {
     try {
         const response = await Data.search(req.body.search)
+        for (let company of response.hits) {
+            company._source['Grade'] = grade(company._source)
+        }
         return res.status(200).send(response)
     }
     catch (e) {
         res.status(400).send({ message: e })
     }
 })
+
+function grade(company) {
+    const grades = {
+        'F': 100000000000000,
+        'F': 30000000000,
+        'D-': 25000000000,
+        'D': 15000000000,
+        'D+': 6000000000,
+        'C-': 3500000000,
+        'C': 1000000000,
+        'C+': 700000000,
+        'B-': 550000000,
+        'B': 300000000,
+        'B+': 120000000,
+        'A-': 65000000,
+        'A': 40000000,
+        'A+': 25000000
+    }
+
+    function compare(a, b) {
+        if (a['Year'] < b['Year']) {
+            return -1
+        }
+        if (a['Year'] > b['Year']) {
+            return 1
+        }
+        return 0
+    }
+
+    const sortedImpacts = company[' Total GHG Environmental Impact (Scope 1, 2, 3)'].sort(compare)
+    const impact = +sortedImpacts[sortedImpacts.length - 1]['Value'].replaceAll(',', '')
+    for (let i = 0; i < Object.keys(grades).length; i++) {
+        if (grades[Object.keys(grades)[i]] >= impact)
+            continue
+        if (i == 0)
+            return 'F'
+        return Object.keys(grades)[i - 1]
+    }
+    return 'A+'
+}
