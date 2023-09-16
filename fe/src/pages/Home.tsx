b633a4ced09a08eace35287a9a3f07320b5a2317
Ersilda Cako <ersildacako14@gmail.com>
2023-09-16 15:47:38 -0400

    Trying to create tabs
    Creating tabs

--- fe/src/pages/Home.tsx
+++ fe/src/pages/Home.tsx
@@ -5,7 +5,6 @@ import { doGet } from '../Utils'
 import ExploreContainer from '../components/ExploreContainer'
 import './Home.css';
 
-
 const Home: React.FC = () => {
 
     async function test() {
@@ -15,21 +14,21 @@ const Home: React.FC = () => {
 
     return (
         <IonPage>
-            <IonHeader>
+            <IonHeader >
                 <IonToolbar className = 'header' color='success'>
-                    <div id="imgHeader">
+                    <div id="imgheader">
                     <img height="40" src ="logotest.png"/>
                     <IonTitle> Basket</IonTitle>
                     </div>
                 </IonToolbar>
             </IonHeader>
             <IonContent fullscreen={true} className='ion-padding'>
-                <div id="missionHeader">
+                <div id="missionheader">
                     <h1>Our Mission</h1>
                     <p>The goal of this app is to blah blah</p>
                 </div>
 
-                <div id="featuredCompanies">
+                <div id="featuredcompanies">
                     <h1>Featured Companies</h1>
                 </div>
                 
@@ -39,7 +38,7 @@ const Home: React.FC = () => {
                     <IonCardHeader>
                         <div className='card-text'>
                             <div className='card-text-left'>
-                                <IonCardTitle id="companytitle"> Company 1 </IonCardTitle>
+                                <IonCardTitle id="companytitle"> Company Name </IonCardTitle>
                                 <IonCardSubtitle id="industry">Industry</IonCardSubtitle>
                             </div>
                             <div className='card-text-right'>
@@ -48,7 +47,24 @@ const Home: React.FC = () => {
                         </div>
                     </IonCardHeader>
                     </button>
-                    {/* <IonButton className='companybutton' color="success" onClick={test}> Learn more</IonButton> */}
+                </IonCard>
+
+
+                <IonCard>
+                    <button ion-item>
+                    <img className='companylogo' src="amazonlogo.png"/>
+                    <IonCardHeader>
+                        <div className='card-text'>
+                            <div className='card-text-left'>
+                                <IonCardTitle id="companytitle"> Company Name </IonCardTitle>
+                                <IonCardSubtitle id="industry">Industry</IonCardSubtitle>
+                            </div>
+                            <div className='card-text-right'>
+                                <IonCardSubtitle>A</IonCardSubtitle>
+                            </div>
+                        </div>
+                    </IonCardHeader>
+                    </button>
                 </IonCard>
 
 
