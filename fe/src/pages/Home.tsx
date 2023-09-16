316dfaa10145e58a899e5a2429e0676a60788f05
Ersilda Cako <ersildacako14@gmail.com>
2023-09-16 01:06:33 -0400

    Mission & Comp
    Added basic text

--- fe/src/pages/Home.tsx
+++ fe/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { add } from 'ionicons/icons'
 import { doGet } from '../Utils'
 
 
+
 import ExploreContainer from '../components/ExploreContainer'
 import './Home.css';
 
@@ -12,24 +13,38 @@ const Home: React.FC = () => {
         let result = await doGet("test")
         alert(result.data.message)
     }
-
+/*<img alt="logo" height="40" src="logotest.png" />*/ 
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar className = 'header' color='success'>
-                    <IonTitle>AppName</IonTitle>
-                   
+                    <div id="imgHeader">
+                    <img height="40" src ="logotest.png"/>
+                    <IonTitle >AppName</IonTitle>
+                    </div>
                 </IonToolbar>
             </IonHeader>
             <IonContent fullscreen>
+                <div id="missionHeader">
+                <h1>Our Mission</h1>
+                <hr/>
+                <p>The goal of this app is to blah blah</p>
+                </div>
+
+                <div id="featuredCompanies">
+                <h1>Featured Companies</h1>
+                <hr/>
+                
+                </div>
+                
                 <IonHeader collapse="condense">
                     <IonToolbar>
-                        <IonTitle size="large">Blank</IonTitle>
+                        <IonTitle size="large">Hello </IonTitle>
                     </IonToolbar>
                 </IonHeader>
-                <IonFabButton className='poopie' onClick={test}>
+                {/* <IonFabButton className='poopie' onClick={test}>
                     <IonIcon icon={add} />
-                </IonFabButton>
+                </IonFabButton> */}
                 <ExploreContainer />
             </IonContent>
         </IonPage>
