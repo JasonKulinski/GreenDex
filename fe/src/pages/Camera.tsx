bdaf51a19bd82fbc3f64f6ac2e0195d571135248
JasonKulinski <123680245+JasonKulinski@users.noreply.github.com>
2023-09-17 00:03:28 -0400

    lol

--- fe/src/pages/Camera.tsx
+++ fe/src/pages/Camera.tsx
@@ -3,6 +3,8 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/react';
 import { useState, useEffect } from 'react';
 import { isPlatform } from '@ionic/react';
 
+
+
 const CameraPage: React.FunctionComponent = () => {
   return (
     <React.Fragment>
