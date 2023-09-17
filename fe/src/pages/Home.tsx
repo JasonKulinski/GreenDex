d4d26251994a7fca4cf550f9dcd5b0e4a98801b7
= <lucas.bent@maine.edu>
2023-09-16 23:32:03 -0400

    poopie doopie

--- fe/src/pages/Home.tsx
+++ fe/src/pages/Home.tsx
@@ -26,19 +26,24 @@ const Home: React.FC = () => {
                 </IonToolbar>
             </IonHeader>
             <IonContent fullscreen={true} className='ion-padding'>
-                <h1>Welcome to Basket!</h1>
-                <img height="120" src="basket.png" />
-                <h2>Our mission</h2>
-                <p>
-                    With global warming hitting a high rating compared to the past few years, every step of making a change counts. Our mission goes hand-in-hand with environmental sustainability by creating an app designed to implement a decision-based buying shift for consumers towards more sustainable products provided by eco-friendly companies. This app recommends or suggests suitable eco-friendly companies to shift consumers' pockets to benefiting the Earth more, making a small step toward a better future.
-                </p>
+                <div id='content'>
+                    <h1>Welcome to Basket!</h1>
+                    <img height="120" src="basket.png" />
+                    <h1>Our mission</h1>
+                    <p>
+                        With global warming hitting a high rating compared to the past few years, every step of making a change counts.
+                        <br /><br />
+                        Our mission goes hand-in-hand with environmental sustainability by creating an app designed to implement a decision-based buying shift for consumers towards more sustainable products provided by eco-friendly companies.
+                        <br /><br />
+                        This app recommends or suggests suitable eco-friendly companies to shift consumers' pockets to benefiting the Earth more, making a small step toward a better future.
+                    </p>
 
-                <h1>Try out Search:</h1>
+                    <h1>Try out Search:</h1>
 
-                <IonFabButton routerLink='/search'>
-                    <IonIcon icon={search} />
-                </IonFabButton>
-
+                    <IonFabButton routerLink='/search'>
+                        <IonIcon icon={search} />
+                    </IonFabButton>
+                </div>
 
             </IonContent>
         </IonPage>
